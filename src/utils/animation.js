a19be869434a6c6ed2071426506f3c0b77635f60
RanggaAlroy <114912822+RanggaAlroy@users.noreply.github.com>
2024-06-23 13:08:10 +0700

    final

--- src/utils/animation.js
+++ src/utils/animation.js
@@ -1,3 +1,8 @@
+import gsap from "gsap"
+import { ScrollTrigger } from "gsap/all"
+
+gsap.registerPlugin(ScrollTrigger)
+
 export const phoneSizeAnimation  = (timeline, rotationRef, rotationState, firstTarget, secondTarget, animationProps) => {
     timeline.to(rotationRef.current.rotation, {
       y: rotationState,
@@ -22,4 +27,16 @@ export const phoneSizeAnimation  = (timeline, rotationRef, rotationState, firstT
       },
       '<'
     )
+  }
+
+  export const animationGSAP = (target, animationProps, scrollProps) => {
+    gsap.to(target, {
+      ... animationProps,
+      scrollTrigger: {
+        trigger: target, 
+        toggleActions: 'restart reverse restart reverse',
+        start: 'top 85%',
+        ...scrollProps
+      }
+    })
   }
